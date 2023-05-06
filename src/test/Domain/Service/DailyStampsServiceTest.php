12f2d79bbba61eb99048c98f795f37d8fec69dd9
 Shin Yamaga <s_yamaga@codmon.co.jp>
2023-05-06 11:28:36 +0900

    例外テストのメッセージチェックを追加

--- src/test/Domain/Service/DailyStampsServiceTest.php
+++ src/test/Domain/Service/DailyStampsServiceTest.php
@@ -33,11 +33,26 @@ class DailyStampsServiceTest extends TestCase
     {
         //then
         $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('従業員IDと処理対象の打刻情報が一致しません');
         //given
         $stamp_1 = Stamp::create(1, 1, 1, '2023-04-01', '2023-04-01 10:00:00');
         $stamps = new Stamps([$stamp_1]);
         $InvalidEmployeeId = 2;
         $date = '2023-04-01';
+        //when
+        $this->service->lastStatus($InvalidEmployeeId, $date, $stamps);
+    }
+
+    public function test_lastStatus_日付が不正値()
+    {
+        //then
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('日付と処理対象の打刻情報が一致しません');
+        //given
+        $stamp_1 = Stamp::create(1, 1, 1, '2023-04-01', '2023-04-01 10:00:00');
+        $stamps = new Stamps([$stamp_1]);
+        $InvalidEmployeeId = 2;
+        $date = '2023-04-02';
         //when
         $this->service->lastStatus($InvalidEmployeeId, $date, $stamps);
     }
